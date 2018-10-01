af6e7513d7a64988b50f532eec8b64c0acf1d2b0
NajaNaja89 <najanaja89@gmail.com>
2018-10-01 18:13:35 +0600

    Second task lenth of ticket

--- CSharpHomework2Module03.git/Program.cs
+++ CSharpHomework2Module03.git/Program.cs
@@ -13,21 +13,41 @@ namespace CSharpHomework2Module03.git
             while (true) {
                 string menu = "";
                 Console.WriteLine("Press 1 to calculate count of spaces in strings");
+                Console.WriteLine("Press 2 to calculate happy number of ticket");
                 menu = Console.ReadLine();
 
-                if (menu == "1") {
+                if (menu == "1")
+                {
                     Console.WriteLine("Enter string, press '.' start calculate count of spaces ");
+
                     string temp = "";
                     string countOfSpace = "";
                     int count = 0;
+
                     while (temp != ".")
                     {
                         temp = Console.ReadLine();
                         countOfSpace += temp;
                         count = countOfSpace.Count(Char.IsWhiteSpace);
                     }
+
                     Console.WriteLine("Entered string is " + countOfSpace);
                     Console.WriteLine("Count of spaces is {0}", count);
+                }
+
+                else if (menu == "2")
+                {
+                    string ticketNum = "";
+
+                    while (ticketNum.Length != 6 )
+                    {
+                        Console.WriteLine("Enter number of ticket (6 digits) ");
+                        ticketNum = Console.ReadLine();
+                        if (ticketNum.Length != 6) Console.WriteLine("Count of digits must equal 6!");
+                    }
+                 
+
+                  
                 }
             }
             Console.ReadLine();
