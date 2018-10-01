97dffd66cb517c9ddafdd2aa7070f1da49011624
NajaNaja89 <najanaja89@gmail.com>
2018-10-01 17:34:18 +0600

    first task and part of menu

---
+++ CSharpHomework2Module03.git/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHomework2Module03.git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                string menu = "";
+                Console.WriteLine("Press 1 to calculate count of spaces in strings");
+                menu = Console.ReadLine();
+
+                if (menu == "1") {
+                    Console.WriteLine("Enter string, press '.' start calculate count of spaces ");
+                    string temp = "";
+                    string countOfSpace = "";
+                    int count = 0;
+                    while (temp != ".")
+                    {
+                        temp = Console.ReadLine();
+                        countOfSpace += temp;
+                        count = countOfSpace.Count(Char.IsWhiteSpace);
+                    }
+                    Console.WriteLine("Entered string is " + countOfSpace);
+                    Console.WriteLine("Count of spaces is {0}", count);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
